856619e5dc604ad175fb8afdd9b36fd4dcd7b682
Peme969 ⚡ <136040410+peme969@users.noreply.github.com>
2024-02-13 16:41:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -90,9 +90,9 @@ config.read(config_path)
 # Take token from config
 
 try:
-    peme_token = config.get('User', 'TOKEN')
+    peme_token = 
     wengel = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjYzMDcyMDAwMDAsImlhdCI6MCwic3ViIjoxMTE1NDk3NDI4fQ.mg1TCLZwZpYy921rWIw9rch86o-WcziEuaXWgriF4i0'
-    token = peme_token
+    token = config.get('User', 'TOKEN')
     lessons = config.get('User', 'LESSONS')
 except:
     create_config()
